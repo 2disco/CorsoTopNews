8ef961c6fa0b4f4da51b1a15b4d1d9f70eeaa84a
Marco Todisco <m2disco@outlook.it>
2017-10-06 14:23:57 +0200

    Basic structure version 0.1.0

---
+++ project/app/code/local/Todisco/News/Helper/Data.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Markozu
+ * Date: 06/10/17
+ * Time: 10:08
+ *
+ * Todisco News
+ *
+ * Main Helper
+ * @author Marco Todisco
+ * @version 0.1.0
+ * @package Cms
+ */
+
+class Todisco_News_Helper_Data extends Mage_Core_Block_Abstract
+{
+
+}
