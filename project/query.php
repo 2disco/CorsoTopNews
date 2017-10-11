7e45ece8d2afc5607249a93b8d8592abbbd8173b
Marco Todisco <m2disco@outlook.it>
2017-10-11 17:53:06 +0200

    Category Grid

--- project/query.php
+++ project/query.php
@@ -3,19 +3,19 @@
 require_once ('app/Mage.php');
 Varien_Profiler::enable();
 Mage::setIsDeveloperMode(true);
-ini_set('display_errors','1');
+ini_set('display_errors',1);
 umask(0);
 Mage::app();
 
 //create category
 //$category = Mage::getModel('todisco_news/category');
 //$category->setCode('top');
-//$category->setName('Think Open');
-//$category->setStatus(true);
+//$category->setName('Assago');
+//$category->setStatus(1);
 //$category->save();
-
-//load the catogory
-//$category = Mage::getModel('todisco_news/category')->load(1);
+//
+////load the category
+$category = Mage::getModel('todisco_news/category')->load(1);
 //echo 'Category<hr>';
 //Zend_Debug::dump($category);
 //echo '<hr>';
